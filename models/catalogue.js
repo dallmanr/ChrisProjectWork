0670dff8caeffac2a3d91bd382613b40b71c7f19
Richard Dallman <dallmanr@users.noreply.github.com>
2017-10-19 21:00:48 +0100

    More page templates added
    Added pages for adding a new car to the catalogue.
    A page for filling out service details for a selected car.
    Some work on the models for the Mongo database.

--- models/catalogue.js
+++ models/catalogue.js
@@ -2,9 +2,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var catalogue = new Schema({
-  title: String,
-  price: Number,
-  likes: Number
+  remarks: String,
+  cat_number: {Integer, required: true},
+  ltd_edition: String,
+  car_manufactuer: {String, required: true},
+  manufacturer_model: {String, required: true},
+  car_number: Integer,
+  colour: {String, required: true},
+  type: {String, required: true},
+  model_manufacturer: {String, required: true},
+  cost: {Integer, required: true},
+  received: {Date, required: true},
+  source: {String, required: true},
+  make: {String, required: true},
+  year: Date
 });
 
 module.exports = mongoose.model('Catalogue', catalogue);
--- models/serviceHistory.js
+++ models/serviceHistory.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var catalogue = new Schema({
-  type: String,
-  products: [{
-    type: ObjectId,
-    ref: 'Catalogue'
-  }]
+  service_type: {String, required: true},
+  service_details: {String, required: true},
+  car: [{type: ObjectId, ref: 'Catalogue'}]
 });
 
 module.exports = mongoose.model('ServiceHistory', serviceHistory);
